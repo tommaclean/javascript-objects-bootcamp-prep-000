a63467a0e91b542d037e7ad48e76478567d42933
Tom MacLean <tom.j.maclean@gmail.com>
2019-02-17 21:17:18 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = {};
+var playlist = { artistName: "songTitle" };
 
 function updatePlaylist(playlist, artistName, songTitle) {
   Object.assign({}, playlist, { artistName: `songTitle` })
